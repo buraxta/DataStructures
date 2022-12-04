c0c862ad6d10faa999b0f9988357ac9e7f4a9927
Burak Güllüler <burakgulluler@gmail.com>
2022-12-04 17:52:47 +0300

    AVL Tree - Height

--- src/AVLTree/AVLTree.java
+++ src/AVLTree/AVLTree.java
@@ -2,6 +2,7 @@ package AVLTree;
 
 public class AVLTree {
     private class AVLNode{
+        private int height;
         private int value;
         private AVLNode leftChild;
         private AVLNode rightChild;
@@ -30,6 +31,15 @@ public class AVLTree {
         }else {
             root.rightChild = insert(root.rightChild, value);
         }
+
+        root.height = Math.max(
+                height(root.leftChild),
+                height(root.rightChild)) +1;
+
         return root;
+    }
+
+    private int height(AVLNode node){
+        return (node == null) ? -1 : node.height;
     }
 }
